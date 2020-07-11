1744af280174f8609feb768bd28abc02296089fb
AnoodALhjaj <Anood8Alhjaj@gmail.com>
2020-07-11 04:58:19 +0300

    fc

---
+++ RadForm1.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace DataMining_
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RadForm1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.fluentTheme1 = new Telerik.WinControls.Themes.FluentTheme();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radTextBox1 = new Telerik.WinControls.UI.RadTextBox();
+            this.radTextBox2 = new Telerik.WinControls.UI.RadTextBox();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.fluentDarkTheme1 = new Telerik.WinControls.Themes.FluentDarkTheme();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radButton3 = new Telerik.WinControls.UI.RadButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radButton4 = new Telerik.WinControls.UI.RadButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yassin_ MagdySoliman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(1056, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "أدخل القيم المراد حسابها";
+            // 
+            // radButton1
+            // 
+            this.radButton1.EnableTheming = false;
+            this.radButton1.Font = new System.Drawing.Font("MS PGothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radButton1.Location = new System.Drawing.Point(842, 99);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(174, 47);
+            this.radButton1.TabIndex = 1;
+            this.radButton1.Text = "<html><span style=\"font-size: medium; font-family: yassin_ magdysoliman\">أضافة</s" +
+    "pan></html>";
+            this.radButton1.ThemeName = "FluentDark";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radTextBox1
+            // 
+            this.radTextBox1.AutoSize = false;
+            this.radTextBox1.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radTextBox1.Location = new System.Drawing.Point(743, 152);
+            this.radTextBox1.Multiline = true;
+            this.radTextBox1.Name = "radTextBox1";
+            this.radTextBox1.ReadOnly = true;
+            // 
+            // 
+            // 
+            this.radTextBox1.RootElement.Enabled = false;
+            this.radTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.radTextBox1.Size = new System.Drawing.Size(488, 336);
+            this.radTextBox1.TabIndex = 3;
+            this.radTextBox1.ThemeName = "EvalFormTheme";
+            // 
+            // radTextBox2
+            // 
+            this.radTextBox2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radTextBox2.Location = new System.Drawing.Point(1022, 104);
+            this.radTextBox2.Name = "radTextBox2";
+            this.radTextBox2.Size = new System.Drawing.Size(209, 37);
+            this.radTextBox2.TabIndex = 0;
+            this.radTextBox2.ThemeName = "FluentDark";
+            this.radTextBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.radTextBox2_KeyPress);
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.radLabel1.AutoScroll = true;
+            this.radLabel1.AutoSize = false;
+            this.radLabel1.BorderVisible = true;
+            this.radLabel1.Font = new System.Drawing.Font("STV", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radLabel1.Location = new System.Drawing.Point(12, 12);
+            this.radLabel1.Name = "radLabel1";
+            // 
+            // 
+            // 
+            this.radLabel1.RootElement.AutoSize = true;
+            this.radLabel1.RootElement.ScaleTransform = new System.Drawing.SizeF(0F, 0F);
+            this.radLabel1.RootElement.UseDefaultDisabledPaint = true;
+            this.radLabel1.Size = new System.Drawing.Size(724, 557);
+            this.radLabel1.TabIndex = 4;
+            this.radLabel1.Text = "<html>النتائج عرض سيتم هنا</html>";
+            this.radLabel1.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radLabel1.ThemeName = "FluentDark";
+            // 
+            // radButton2
+            // 
+            this.radButton2.EnableTheming = false;
+            this.radButton2.Font = new System.Drawing.Font("MS PGothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radButton2.Location = new System.Drawing.Point(743, 12);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(174, 47);
+            this.radButton2.TabIndex = 2;
+            this.radButton2.Text = "<html><span style=\"font-size: medium; font-family: yassin_ magdysoliman\">عرض النت" +
+    "يجة</span></html>";
+            this.radButton2.ThemeName = "FluentDark";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("AlWatanHeadlines-Bold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(858, 491);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(373, 46);
+            this.label2.TabIndex = 5;
+            this.label2.Text = " Enter لا تحتاج لضغط زر الأضافة في كل مرة يمكنك ضغط ";
+            // 
+            // radButton3
+            // 
+            this.radButton3.EnableTheming = false;
+            this.radButton3.Font = new System.Drawing.Font("MS PGothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.radButton3.Location = new System.Drawing.Point(757, 99);
+            this.radButton3.Name = "radButton3";
+            // 
+            // 
+            // 
+            this.radButton3.RootElement.BorderHighlightThickness = 1;
+            this.radButton3.Size = new System.Drawing.Size(65, 47);
+            this.radButton3.TabIndex = 3;
+            this.radButton3.Text = "<html><span style=\"font-size: medium; font-family: yassin_ magdysoliman\">مسح</spa" +
+    "n></html>";
+            this.radButton3.ThemeName = "FluentDark";
+            this.radButton3.Click += new System.EventHandler(this.radButton3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("AlWatanHeadlines-Bold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(994, 591);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(228, 46);
+            this.label3.TabIndex = 6;
+            this.label3.Text = " أَضافة ملف بيانات بصيغة ملف أكسل";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("AlWatanHeadlines-Bold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(684, 640);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(361, 46);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "عند أختيار ملف أكسل يفضل وضع جميع القيم في عمود واحد ";
+            // 
+            // radButton4
+            // 
+            this.radButton4.EnableTheming = false;
+            this.radButton4.Font = new System.Drawing.Font("STV", 11.25F, System.Drawing.FontStyle.Bold);
+            this.radButton4.Location = new System.Drawing.Point(504, 585);
+            this.radButton4.Name = "radButton4";
+            this.radButton4.Size = new System.Drawing.Size(174, 62);
+            this.radButton4.TabIndex = 3;
+            this.radButton4.Text = resources.GetString("radButton4.Text");
+            this.radButton4.ThemeName = "FluentDark";
+            this.radButton4.Click += new System.EventHandler(this.radButton4_Click);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("STV", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(705, 600);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(283, 34);
+            this.label4.TabIndex = 7;
+            this.label4.Text = " Xslx.أسم الملف";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Chiller", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(8, 662);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 24);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Made By Anood Alhjaj";
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1261, 695);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.radButton4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radButton3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radTextBox1);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radTextBox2);
+            this.Controls.Add(this.radButton1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("STV", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "حساب مقاييس التشتت والنزعة المركزية";
+            this.ThemeName = "FluentDark";
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radTextBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private Telerik.WinControls.Themes.FluentTheme fluentTheme1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox1;
+        private Telerik.WinControls.UI.RadTextBox radTextBox2;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.Themes.FluentDarkTheme fluentDarkTheme1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private System.Windows.Forms.Label label2;
+        private Telerik.WinControls.UI.RadButton radButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label5;
+        private Telerik.WinControls.UI.RadButton radButton4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ RadForm1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+namespace DataMining_
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        List<double> numbers = new List<double>();
+        double[] terms;
+
+        int Repeated = 1;
+        int itemrep = 1;
+        double[] Mo;
+        String num;
+        public RadForm1()
+        {
+            InitializeComponent();
+        }
+
+        //المنوال
+        public List<double> mostFrequent(double[] arr)
+        {
+            Repeated = 1;
+            itemrep = 1;
+            List<double> mode = new List<double>();
+            List<double> res = new List<double>();
+            // Sort the array 
+            Array.Sort(arr);
+
+
+            //compare
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                double a = arr[i];
+                double b = arr[i + 1];
+
+                if (a == b)
+                {
+                    itemrep++;
+                    if (mode.Contains(a))
+                    {
+                       
+
+                        if (itemrep > Repeated)
+                        {
+                            Repeated = itemrep;
+                        }
+
+                    }
+                    else
+                    {
+                        if (itemrep > Repeated)
+                        {
+                            Repeated = itemrep;
+                        }
+                        itemrep = 1;
+                        mode.Add(a);
+                       
+                    }
+
+                }
+            }
+
+            int count = 1;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    count++;
+                    if (count == Repeated)
+                    {
+
+                        res.Add(arr[i]);
+                    }
+                }
+                else { count = 1; }
+
+
+
+            }
+
+            
+            return res;
+
+        }
+
+
+        //الانحراف المعياري
+
+        String Standard_deviation(double[] terms, double Mean)
+        {
+            string num = "";
+            double Sum = 0;
+            for (int i = 0; i < terms.Length; i++)
+            {
+                Sum = Sum + (terms[i] * terms[i]);
+            }
+            double divide = Math.Round((double)(Sum) / terms.Length, 2);
+            double div2 = Math.Round((Mean * Mean), 2);
+            num = (Math.Round(Math.Sqrt(divide - div2), 2)).ToString();
+
+
+            return num;
+        }
+        //المركز
+        String MidRange(double[] terms)
+        {
+            String num = "";
+            num = (Math.Round((((double)terms.Max() + terms.Min()) / 2), 2)).ToString();
+            return num;
+        }
+        //التباين
+
+        String variance(double[] terms, double Mean)
+        {
+
+
+            string num = "";
+            double Sum = 0;
+            for (int i = 0; i < terms.Length; i++)
+            {
+                Sum = Sum + Math.Pow((terms[i] - Mean), 2);
+            }
+            double divide = Math.Round((double)(Sum) / terms.Length, 4);
+            num = (divide).ToString();
+
+
+            return num;
+
+
+        }
+        void prepare()
+        {
+            terms = numbers.ToArray();
+            radTextBox1.Text = "";
+            for (int run = 0; run < terms.Length; run++)
+            {
+                if (run == 0)
+                {
+                    radTextBox1.Text = radTextBox1.Text + terms[run].ToString();
+
+                }
+                else
+                {
+                    radTextBox1.Text = radTextBox1.Text + "," + terms[run].ToString();
+                }
+            }
+
+            radTextBox2.Text = "";
+        }
+
+        private void radButton1_Click(object sender, EventArgs e)
+        {
+            if (radTextBox2.Text != "")
+            {
+                numbers.Add(double.Parse(radTextBox2.Text));
+                prepare();
+            }
+        }
+
+        //الوسط
+        double Mean(double[] terms)
+        {
+            
+            if (terms.Length % 2 == 0)
+            {
+                double men = Math.Round((double)(terms[(terms.Length / 2) - 1] + terms[(terms.Length / 2)]) / 2, 2);
+
+
+                return  men;
+            }
+            else
+            {
+               return terms[((terms.Length - 1) / 2)];
+            }
+        }
+
+        private void radTextBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == 13)
+            {
+                if (!radTextBox2.AcceptsReturn && radTextBox2.Text.Length > 0)
+                {
+                    radButton1.PerformClick();
+                }
+            }
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+        (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            
+        }
+
+        private void radButton2_Click(object sender, EventArgs e)
+        {
+            String deviation = "";
+            double mean;
+            //الوسيط الحسابي
+            double avarge = Math.Round((double)(terms.Sum()) / terms.Length, 2);
+            //الانحراف المعياري
+
+            deviation = Standard_deviation(terms, avarge);
+            //المركز
+            mean = Mean(terms);
+            //التباين
+            string va = variance(terms, avarge);
+
+
+            //المنوال
+
+            Mo = mostFrequent(terms).ToArray();
+            
+            num = "";
+            
+                for (int run = 0; run < Mo.Length; run++)
+                {
+                    if (run == 0)
+                    {
+                        num = num + Mo[run].ToString();
+
+                    }
+                    else
+                    {
+                        num = num + "," + Mo[run].ToString();
+                    }
+                }
+
+             if(num==""){
+                for (int run = 0; run < terms.Length; run++)
+                {
+                    if (run == 0)
+                    {
+                        num = num + terms[run].ToString();
+
+                    }
+                    else
+                    {
+                        num = num + "," + terms[run].ToString();
+                    }
+                }
+            }
+
+
+             radLabel1.Text = "(الوسط الحسابي) Avrage =" + "  " + avarge.ToString() + "\n" + "(الوسيط) Median =     " + mean.ToString() + "\n" + "(المنوال)  Mode=  [" + num + "]" + "\n" + " (الأنحراف المعياري) Standard deviation =  [" + deviation + "]" + "\n" + "(التباين) variance =  [" + va + "]" + "\n" + "(المركز) MidRange =  [" + MidRange(terms) + "]" + "\n" + "(المدى) Range =  [" + (terms.Max() - terms.Min()) + "]" + "\n" + "(معامل التشتت) CV =  [" + Math.Round(( (double.Parse(deviation)/avarge) *100),2) + "]";
+        }
+
+        private void radButton3_Click(object sender, EventArgs e)
+        {
+            if (terms != null)
+            {
+                numbers.Clear();
+                Array.Clear(terms, 0, terms.Length);
+                if (Mo != null)
+                {
+                    Array.Clear(Mo, 0, Mo.Length);
+                }
+                num = "";
+                radLabel1.Text = "هنا سيتم عرض النتائج";
+                radTextBox1.Text = "";
+                label4.Text = "Xslx.أسم الملف";
+            }
+        }
+    
+        void CovertCv(String fn)
+        {
+            // Get the Excel application object.
+            var excel_app = new Microsoft.Office.Interop.Excel.Application();
+          
+
+            // Make Excel visible (optional).
+            excel_app.Visible = false;
+
+            // Open the workbook read-only.
+            Excel.Workbook workbook = excel_app.Workbooks.Open(
+                fn,
+                Type.Missing, true, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            // Get the first worksheet.
+            Excel.Worksheet sheet = (Excel.Worksheet)workbook.Sheets[1];
+
+            // Get the values only.
+            SetTitleAndListValues(sheet, 1, 1);
+            
+
+            // Close the workbook without saving changes.
+            workbook.Close(false, Type.Missing, Type.Missing);
+
+            // Close the Excel server.
+            excel_app.Quit();
+        }
+        private void SetTitleAndListValues(Excel.Worksheet sheet,
+    int row, int col)
+        {
+            Excel.Range range;
+
+            // Set the title.
+            range = (Excel.Range)sheet.Cells[row, col];
+          
+
+            // Get the values.
+            // Find the last cell in the column.
+            range = (Excel.Range)sheet.Columns[col, Type.Missing];
+            Excel.Range last_cell =range.get_End(Excel.XlDirection.xlDown);
+
+            // Get a Range holding the values.
+            Excel.Range first_cell =
+                (Excel.Range)sheet.Cells[row , col];
+            Excel.Range value_range =
+                (Excel.Range)sheet.get_Range(first_cell, last_cell);
+
+            // Get the values.
+            object[,] range_values = (object[,])value_range.Value2;
+
+            // Convert this into a 1-dimensional array.
+            // Note that the Range's array has lower bounds 1.
+            int num_items = range_values.GetUpperBound(0);
+            double[] values1 = new double[num_items];
+            for (int i = 0; i < num_items; i++)
+            {
+                values1[i] = (double)range_values[i + 1, 1];
+               numbers.Add(values1[i]);
+            }
+            prepare();
+            // Display the values in the ListBox.
+            
+        }
+        
+        
+
+        private void radButton4_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"E:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "xlsx",
+                Filter = "xlsx files (*.xlsx)|*.xlsx",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label4.Text = openFileDialog1.FileName;
+                CovertCv(openFileDialog1.FileName);
+            }  
+        }
+
+        
+    }
+
+
+
+    
+
+}
